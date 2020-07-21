7f51ccdb7542af63e596e167fe84d1ee4921bafb
gadekaramit <32438740+gadekaramit@users.noreply.github.com>
2020-07-21 12:12:49 +0530

    Add files via upload

---
+++ GoodBye.js
@@ -0,0 +1,14 @@
+
+
+(
+function (window) {
+	var byeSpeaker ={};
+	var speakWord = "Good Bye";
+
+	function speak(name) {
+		console.log(speakWord + " " + name);
+	}
+
+window.byeSpeaker=byeSpeaker;
+
+})(window);
---
+++ Hello.js
@@ -0,0 +1,15 @@
+
+
+
+
+(
+function (window) {
+	var helloSpeaker ={};
+	var speakWord = "Hello";
+
+	function speak(name) {
+		console.log(speakWord + " " + name);
+	}
+window.helloSpeaker=helloSpeaker;
+
+})(window);
